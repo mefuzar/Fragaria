5195fa7e49e3d5f26953132fd221abba2f9d20c1
mefuzar <85771020+mefuzar@users.noreply.github.com>
2021-07-03 03:40:04 +0300

    Update uversion.h

--- icu/unicode/uversion.h
+++ icu/unicode/uversion.h
@@ -37,7 +37,7 @@
 /** source/tools/genren/genren.pl - use this script according to the README   */
 /**                    in that folder                                         */
 
-#include "unicode/umachine.h"
+#include "umachine.h"
 
 /** The standard copyright notice that gets compiled into each library. 
  *  This value will change in the subsequent releases of ICU
