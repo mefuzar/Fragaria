0f30971fbc99a09165a7610885a6ed840d263e98
mefuzar <85771020+mefuzar@users.noreply.github.com>
2021-07-03 03:32:13 +0300

    Update NSStringICUAdditions.m

--- NSStringICUAdditions.m
+++ NSStringICUAdditions.m
@@ -20,8 +20,8 @@ typedef struct URegularExpression URegularExpression;
 
 #define U_HIDE_DRAFT_API 1
 #define U_DISABLE_RENAMING 1
-#import <unicode/uregex.h>
-#import <unicode/ustring.h>
+#import <icu/unicode/uregex.h>
+#import <icu/unicode/ustring.h>
 
 @interface NSString (NSStringICUPrivateAdditions)
 
